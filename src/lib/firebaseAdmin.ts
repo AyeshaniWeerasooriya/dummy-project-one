23a6790d309786bcb0391da0bf8c9eac8e220172
AyeshaniWeerasooriya <ayeshani.weerasooriya@gmail.com>
2025-09-26 14:26:48 +0530

    Update firebaseAdmin.ts

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -11,14 +11,3 @@ if (!admin.apps.length) {
 }
 
 export default admin;
-
-// import * as admin from "firebase-admin";
-// import serviceAccount from "./firebase-service-account.json";
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-//   });
-// }
-
-// export default admin;
