e2e93b9a6d237a0fffabda6f3887f81b9f2ff3a2
AyeshaniWeerasooriya <ayeshani.weerasooriya@gmail.com>
2025-09-26 11:50:41 +0530

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,10 @@ export default function Home() {
   const [checkingAuth, setCheckingAuth] = useState(true); // Track loading state
 
   useEffect(() => {
+    console.log(
+      "Firebase authDomain:",
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+    );
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         router.push("/user-editor");
